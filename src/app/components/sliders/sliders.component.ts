44fe8c7e5742444a33072b99ddd585635bf9b177
Brayan Camargo <brayan440@hotmail.com>
2020-02-03 22:32:45 -0500

    Carrusel config

--- src/app/components/sliders/sliders.component.ts
+++ src/app/components/sliders/sliders.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sliders',
   templateUrl: './sliders.component.html',
-  styleUrls: ['./sliders.component.sass']
+  styleUrls: ['./sliders.component.scss']
 })
 export class SlidersComponent implements OnInit {
 
